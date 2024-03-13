c3356b7b8c1b072c469635309c0f0f539cb35d8c
Jose Escalona <JEscalona@lan.xven.com.ve>
2024-03-13 08:34:33 -0400

    new

---
+++ LOGIN/conexion.php
@@ -0,0 +1,13 @@
+<?php
+
+$server = "localhost";
+$DB = "pj";
+$userPJ = "root";
+$passwodPJ = "";
+$portsPJ = "3306";
+
+//CREATE CONEXION
+$conn = new mysqli($server, $DB, $userPJ, $passwodPJ, $portsPJ);
+
+
+?>
