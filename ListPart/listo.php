999e054db29a940bb3954ec8e41d8b0dcdf847c6
Jose Escalona <JEscalona@lan.xven.com.ve>
2024-03-18 13:53:07 -0400

    new

--- ListPart/listo.php
+++ ListPart/listo.php
@@ -1,56 +1,149 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
+
 <head>
-    <meta charset="UTF-8">
+<meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="author" content="Marco Robles">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
-    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <link rel="stylesheet" href="http://localhost/proyecto-j/CSS/style.css">
+    <link rel="stylesheet" href="http://localhost/proyecto-j/CSS/panel.css">
+    <link rel="icon" href="http://localhost/proyecto-j/IMG/logo.png">
+    <title>ListPart</title>
+
+    <!-- Bootstrap core CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-   
-    <title>Document</title>
+
 </head>
+
 <body>
-    
-            <style>
-                table, th td{
-                    border:  1px solid;
-                }
+    <main>
+        <div class="container py-4 text-center">
+            <h2>DATA</h2>
+
+            <div class="row g-4">
+
+                <div class="col-md-4 col-xl-5"></div>
+
+                <div class="col-6 col-md-1 text-end">
+                    <label for="campo" class="col-form-label">Buscar: </label>
+                </div>
+                <div class="col-6 col-md-3 text-end">
+                    <input type="text" name="campo" id="campo" class="form-control">
+                </div>
+            </div>
             
-                table {
-                    width: 80%;
-                    border-collapse: collapse;
-                }  
+<br>
+<br>
+<br>
 
-            </style>
+            <div class="row py-4">
+                <div class="col">
+                    <table class="table table-sm table-bordered table-striped">
+                        <thead>
+                            <th class="sort asc">Num. empleado</th>
+                            <th class="sort asc">Nombre</th>
+                            <th class="sort asc">Apellido</th>
+                            <th class="sort asc">Fecha nacimiento</th>
+                            <th class="sort asc">Fecha ingreso</th>
+                            <th></th>
+                            <th></th>
+                        </thead>
 
-            <?php
-            include '../ListPart/modelo/conexion.php';
-            include '../ListPart/controlador/filter.php';
-            ?>
+                        <!-- El id del cuerpo de la tabla. -->
+                        <tbody id="content">
 
-            <form action="" method-"POST">
-                <label for="campo">Buscar:</label>
-                <input type="text" name="campo" id="campo">
-            </form>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
 
+            <div class="row justify-content-between">
 
+                <div class="col-12 col-md-4">
+                    <label id="lbl-total"></label>
+                </div>
 
-            <table>
-                <thead>
-                        <th class="col">PartNumber</th>
-                        <th class="col">E/A/S</th> 
-                        <th class="col">Model</th>
-                        <th class="col">Segment</th>
-                        <th class="col">Mo/Co</th>
-                        <th class="col">Description</th>
-                        <th class="col">Yield</th>                            <th class="col">Region</th>
-                </thead>
-            </table>
+                <div class="col-12 col-md-4" id="nav-paginacion"></div>
 
-            <tbody id="content">
+                <input type="hidden" id="pagina" value="1">
+                <input type="hidden" id="orderCol" value="0">
+                <input type="hidden" id="orderType" value="asc">
 
-            </tbody>
+            </div>
+        </div>
+    </main>
 
+    <script>
+        // Llamando a la función getData() al cargar la página
+        document.addEventListener("DOMContentLoaded", getData);
 
+        // Función para obtener datos con AJAX
+        function getData() {
+            let input = document.getElementById("campo").value
+            let num_registros = document.getElementById("num_registros").value
+            let content = document.getElementById("content")
+            let pagina = document.getElementById("pagina").value || 1;
+            let orderCol = document.getElementById("orderCol").value
+            let orderType = document.getElementById("orderType").value
+
+            let formaData = new FormData()
+            formaData.append('campo', input)
+            formaData.append('registros', num_registros)
+            formaData.append('pagina', pagina)
+            formaData.append('orderCol', orderCol)
+            formaData.append('orderType', orderType)
+
+            fetch("load.php", {
+                    method: "POST",
+                    body: formaData
+                })
+                .then(response => response.json())
+                .then(data => {
+                    content.innerHTML = data.data
+                    document.getElementById("lbl-total").innerHTML = `Mostrando ${data.totalFiltro} de ${data.totalRegistros} registros`;
+                    document.getElementById("nav-paginacion").innerHTML = data.paginacion
+
+                    // Si la página actual no tiene resultados, ajustar la paginación para mostrar la primera página
+                    if (data.data.includes('Sin resultados') && parseInt(pagina) !== 1) {
+                        nextPage(1); // Ir a la primera página
+                    }
+                })
+                .catch(err => console.log(err))
+        }
+
+        // Función para cambiar de página
+        function nextPage(pagina) {
+            document.getElementById('pagina').value = pagina
+            getData()
+        }
+
+        // Función para ordenar columnas
+        function ordenar(e) {
+            let elemento = e.target;
+            let orderType = elemento.classList.contains("asc") ? "desc" : "asc";
+
+            document.getElementById('orderCol').value = elemento.cellIndex;
+            document.getElementById("orderType").value = orderType;
+            elemento.classList.toggle("asc");
+            elemento.classList.toggle("desc");
+
+            getData()
+        }
+
+        // Event listeners para los eventos de cambio en el campo de entrada y el select
+        document.getElementById("campo").addEventListener("keyup", getData);
+        document.getElementById("num_registros").addEventListener("change", getData);
+
+        // Event listener para ordenar las columnas
+        let columns = document.querySelectorAll(".sort");
+        columns.forEach(column => {
+            column.addEventListener("click", ordenar);
+        });
+    </script>
+
+    <!-- Bootstrap core JS -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
+
 </html>
--- ListPart/listparts.php
+++ ListPart/listparts.php
@@ -1,96 +1,76 @@
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="author" content="Marco Robles">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
-    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="stylesheet" href="http://localhost/proyecto-j/CSS/list.css">
+    <link rel="stylesheet" href="http://localhost/proyecto-j/CSS/style.css">
     <link rel="stylesheet" href="http://localhost/proyecto-j/CSS/panel.css">
-    <link rel="icon" href="../IMG/logo.png">
-    <title>ListParts</title>
+    <link rel="icon" href="http://localhost/proyecto-j/IMG/logo.png">
+    <title>ListPart</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
 </head>
-<body style="background: #f3f3f3;">
-    
-<!--
-        <header class="header">
-            <nav>
-                <ul class="nav-link">
-                    <div class="logo">
-                        <img src="../IMG/jquery-logo-120 (1).png" alt="">
-                    </div>
-                  
-                    <div style="margin-left: 18px; margin-top: -10%;" class="li-nav">
-                        <li><a href="../index.html"><i style="margin-top: 5px;" class='bx bxs-widget'></i>Dashboard</a></li>
-                        <li><a href="../clientes.html"><i style="margin-top: 5px;" class='bx bxs-user'></i>Clientes</a></li>
-                        <li><a href="../report.html"><i style="margin-top: 5px;" class='bx bxs-report' ></i>Report</a></li>
-                        <li><a href="../devices.html"><i style="margin-top: 5px;" class='bx bxs-printer'></i>Devices</a></li>
-                        <li><a href="../counter.html"><i style="margin-top: 5px;" class='bx bx-slider-alt'></i>Counter</a></li>
-                        <li><a href="../DP.html"><i style="margin-top: 5px;" class='bx bxs-cog'></i>ListParts</a></li>
-                    </div>
-                </ul>
-            </nav>
-        </header>
-        -->
 
 
-           <div class="title">
-                <a href=""><p>ListPart</p></a>
-           </div>
+<body>
+            <header class="header">
+                    <nav>
+                        <ul class="nav-link">
+                            <div class="logo">
+                                <img src="http://localhost/proyecto-j//IMG/jquery-logo-120 (1).png" alt="">
+                            </div>
+                            <li><a href="http://localhost/proyecto-j/index.html"><i class='bx bxs-widget'></i>Dashboard</a></li>
+                            <li><a href="http://localhost/proyecto-j/clientes.html"><i class='bx bxs-user'></i>Clientes</a></li>
+                            <li><a href="http://localhost/proyecto-j/report.html"><i class='bx bxs-report' ></i>Report</a></li>
+                            <li><a href="http://localhost/proyecto-j/devices.html"><i class='bx bxs-printer'></i>Devices</a></li>
+                            <li><a href="http://localhost/proyecto-j/counter.html"><i class='bx bx-slider-alt'></i>Counter</a></li>
+                            <li><a href="http://localhost/proyecto-j//ListPart/listparts.php"><i class='bx bxs-cog'></i>ListParts</a></li>
+                        </ul>
+                    </nav>
+                </header>
 
 
-            <div class="public">
-                <img src="../IMG/XeroxLogo.png" alt="">
+
+    <main>
+        <div class="container py-4 text-center">
+            <h2>ListPart</h2>
+
+            <div class="row g-4">
+
+                <div class="col-md-4 col-xl-5"></div>
+
+                <div class="col-6 col-md-1 text-end">
+                    <label for="campo" class="col-form-label">Buscar: </label>
+                </div>
+                <div class="col-6 col-md-3 text-end">
+                    <input type="text" name="campo" id="campo" class="form-control">
+                    <button type="submit" name-"enviar">Filtrar</button>
+                </div>
             </div>
+            <br>
+            <br>
+            <br>
 
+            <div class="row py-4">
+                <div class="col">
+                    <table class="table table-sm table-bordered table-striped">
+                        <thead>
+                            <th class="sort asc">PartN</th>
+                            <th class="sort asc">EAS</th>
+                            <th class="sort asc">Model</th>
+                            <th class="sort asc">SeG</th>
+                            <th class="sort asc">Mo_Co</th>
+                            <th class="sort asc">DesC</th>
+                            <th class="sort asc">PerF</th>
+                            <th class="sort asc">Region</th>
+                        </thead>
 
-            <?php
-            include "../listpart/modelo/conexion.php";
-            include "../ListPart/controlador/filter.php";
-            ?>
-            <div class="col-lg-3 col-6 text-left">
-                <form action="" method="GET">
-                    <div class="input-group">
-                        <input type="text" name="busqueda" id="filter" class="form-control" placeholder="Filtrar Producto...">
-                        <input type="submit" name="enviar" value="Filtrar">
-                        <div class="input-group-append">
-                        </div>
-                    </div>
-                </form>
-
-            
-            </div>
-          
-
-
-
-            <?php
-           // include '../ListPart/modelo/conexion.php';
-            include '../ListPart/controlador/vistaData.php';
-            
-            ?>
-
-
-           <div style="margin-left: 180px; margin-top: 80px; font-size: 17px;" class="container">
-                <form class="col-3" method="POST">
-           <div class="container-vistas">
-               <!-- <div class="col-25"> -->
-                    <table class="table table-bordered">
-
-                    <thead class="bg-info">
-                        <tr>
-                            <th class="col">PartNumber</th>
-                            <th class="col">E/A/S</th> 
-                            <th class="col">Model</th>
-                            <th class="col">Segment</th>
-                            <th class="col">Mo/Co</th>
-                            <th class="col">Description</th>
-                            <th class="col">Yield</th>
-                            <th class="col">Region</th>
-                        </tr>
-                    </thead>
-                    <tbody>
+                        <tbody >
                         <?php
                         include "modelo/conexion.php";
                         $sql=$conexion->query( "select *from listpart" );
@@ -109,32 +89,15 @@
                         <?php
                         }
                         ?>
-
-                    </tbody>
-                </table>
+                        </tbody>
+                    </table>
+                </div>
             </div>
-            </div>
+
+           
 
 
-
-
-
-
-
-
-
-
-
-
-<br>
-<br>
-<br>
-
-
-
-
-
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-
+   
 </body>
+
 </html>
