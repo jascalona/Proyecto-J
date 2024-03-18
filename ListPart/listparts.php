e943f2bda5894bb03565b604996b618275cd634e
Jose Escalona <JEscalona@lan.xven.com.ve>
2024-03-18 08:38:26 -0400

    new

--- ListPart/listparts.php
+++ ListPart/listparts.php
@@ -6,7 +6,8 @@
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="stylesheet" href="../CSS/panel.css">
+    <link rel="stylesheet" href="http://localhost/proyecto-j/CSS/list.css">
+    <link rel="stylesheet" href="http://localhost/proyecto-j/CSS/panel.css">
     <link rel="icon" href="../IMG/logo.png">
     <title>ListParts</title>
 </head>
@@ -36,6 +37,26 @@
            <div class="title">
                 <a href=""><p>ListPart</p></a>
            </div>
+
+
+            <div class="public">
+                <img src="../IMG/XeroxLogo.png" alt="">
+            </div>
+
+
+            <div class="col-lg-3 col-6 text-left">
+                <form action="" method="POST">
+                    <div class="input-group">
+                        <input type="text" class="form-control" placeholder="Filtrar Producto...">
+                        <div class="input-group-append">
+                            <span class="input-group-text bg-transparent text-primary">
+                            <button type="submit"><i class='bx bx-search-alt'></i></button>                            </span>
+                        </div>
+                    </div>
+                </form>
+            </div>
+          
+
 
             <?php
             include '../ListPart/modelo/conexion.php';
