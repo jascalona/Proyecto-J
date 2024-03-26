1fc2b2b68753fb8a4fb6efe784f2cf44adf05f31
Jose Escalona <JEscalona@lan.xven.com.ve>
2024-03-26 11:51:07 -0400

    new

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <link rel="stylesheet" href="http://localhost/proyecto-j//LOGIN/login.css">
+    <link rel="icon" href="http://localhost/proyecto-j//IMG/logo.png">
+    <title>Login</title>
+</head>
+<body>
+    
+
+        
+     
+
+
