3fbfec451e4a44e3a7e5b598ca292c7691d65207
Jose Escalona <JEscalona@lan.xven.com.ve>
2024-03-26 17:04:54 -0400

    new

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
-    <link rel="stylesheet" href="http://localhost/proyecto-j///LOGIN/login.css">
+    <link rel="stylesheet" href="http://localhost/proyecto-j//LOGIN/login.css">
     <link rel="icon" href="http://localhost/proyecto-j//IMG/logo.png">
     <title>Login</title>
 </head>
@@ -21,15 +21,15 @@
             <div class="container-log">
                 <form action="" method="POST" >
                 
-                    <h4>Socios-Iniciar sesión con su usuario de red</h4>
+                    <p>Socios-Iniciar sesión con su usuario de red</p>
 
                     <div class="col-3">
-                            <label for="user">Usuario</label><br>
+                            <label for="user">Usuario</label><br><br>
                             <input type="text" name="usuario" id="usuario" placeholder="Ingrese su Usuario" required>
                         </div>
                     
                     <div class="col-3">
-                        <label for="password">Contraseña</label><br>
+                        <label for="password">Contraseña</label><br><br>
                         <input type="password" name="password" id="password" placeholder="Ingrese su Contraseña" required>
                     </div>
 
