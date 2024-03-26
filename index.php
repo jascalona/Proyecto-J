3f0fe250ff0123bcc0cca6861b05f559b5671f68
Jose Escalona <JEscalona@lan.xven.com.ve>
2024-03-26 16:08:35 -0400

    new

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
-    <link rel="stylesheet" href="http://localhost/proyecto-j//LOGIN/login.css">
+    <link rel="stylesheet" href="http://localhost/proyecto-j///LOGIN/login.css">
     <link rel="icon" href="http://localhost/proyecto-j//IMG/logo.png">
     <title>Login</title>
 </head>
@@ -13,23 +13,27 @@
 
 
         <?php
-        include "Devices/Conn/conexion.php";
+        include "LOGIN/Conn/conexion.php";
         ?>
 
-        <form action="" method="POST" >
+
+
             <div class="container-log">
+                <form action="" method="POST" >
                 
-            <div class="col-3">
-                    <label for="user">Usuario</label>
-                    <input type="text" name="usuario" id="usuario" placeholder="Ingrese su Usuario" required>
-                </div>
-            
-            <div class="col-3">
-                <label for="password">Contraseña</label>
-                <input type="password" name="password" id="password" placeholder="Ingrese su Contraseña" required>
+                    <h4>Socios-Iniciar sesión con su usuario de red</h4>
+
+                    <div class="col-3">
+                            <label for="user">Usuario</label><br>
+                            <input type="text" name="usuario" id="usuario" placeholder="Ingrese su Usuario" required>
+                        </div>
+                    
+                    <div class="col-3">
+                        <label for="password">Contraseña</label><br>
+                        <input type="password" name="password" id="password" placeholder="Ingrese su Contraseña" required>
+                    </div>
+
+                    <button type="submit" name="btn-ingrese" id="btn-ingrese">Ingresar</button>
+                </form>
             </div>
 
-            <button type="submit" name="btn-ingrese" id="btn-ingrese">Ingresar</button>
-
-            </div>
-        </form>
