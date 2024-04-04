19a5133008e9c2d6f3783810023ecb6420dd8d5a
Jose Escalona <JEscalona@lan.xven.com.ve>
2024-04-04 10:27:04 -0400

    new

--- BIGOTT/panel.php
+++ BIGOTT/panel.php
@@ -8,6 +8,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="http://localhost/proyecto-j/CSS/panel.css">
     <link rel="icon" href="../IMG/logo.png">
+    <link rel="stylesheet" href="http://localhost/proyecto-j///CSS/FRAMEWORK/Modal.css">
     <title>BIGOTT</title>
 
         <meta name="viewport" content="width=device-width, initial-scale=1">
--- HUAWEI/panel.php
+++ HUAWEI/panel.php
@@ -7,6 +7,7 @@
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="http://localhost/proyecto-j/CSS/panel.css">
+    <link rel="stylesheet" href="http://localhost/proyecto-j///CSS/FRAMEWORK/Modal.css">
     <link rel="icon" href="../IMG/logo.png">
     <title>HUAWEI</title>
 
